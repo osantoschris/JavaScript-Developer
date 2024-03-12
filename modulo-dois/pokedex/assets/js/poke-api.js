9ab1d2ae64799d1c6fbe117621d3eae7a42a1955
osantoschris <christianoliveira8@gmail.com>
2024-03-11 21:12:33 -0300

    Dinamização das requisições. Implementada a busca pelos 10 primeiros itens da api e, após encontrar, realiza outras requisições para encontrar os detalhes.

--- modulo-dois/pokedex/assets/js/poke-api.js
+++ modulo-dois/pokedex/assets/js/poke-api.js
@@ -1,11 +1,21 @@
 const pokeApi = {}
 
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url).then((response) => response.json())
+}
+
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
 
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
     return fetch(url)
-        .then((response) => response.json()) // Converte o Body em JSON
-        .then((jsonBody) => jsonBody.results)// Busca somente o results do JSON
+        .then((response) => response.json())  // Converte o Body em JSON
+        .then((jsonBody) => jsonBody.results) // Busca somente o results do JSON
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
+
+
+
         .catch((error) => console.error(error))
 };
