14ef854ac5f99414c78f16f2b63f8def916f06f1
osantoschris <christianoliveira8@gmail.com>
2024-05-19 16:31:18 -0300

    trabalhando com mais tipos de dados

--- modulo-tres/tipos-dados.js
+++ modulo-tres/tipos-dados.js
@@ -10,24 +10,51 @@
 
 // object
 
-let x = 10;
+// let x = 10;
 
-let y = { numero: 10}
-console.log(y);
-y.numero = 20
-console.log(y);
+// let y = { numero: 10}
+// console.log(y);
+// y.numero = 20
+// console.log(y);
 
-console.log('Conversão dos tipos de dados, tentativa de coerção:')
+// console.log('Conversão dos tipos de dados, tentativa de coerção:')
 
-console.log(10 + '10');
-console.log('Como não conseguiu resolver, realizar a concatenação. Se utilizarmos outro operador, como o de subtração, realizará a coerção:')
-console.log(25 - '10');
+// console.log(10 + '10');
+// console.log('Como não conseguiu resolver, realizar a concatenação. Se utilizarmos outro operador, como o de subtração, realizará a coerção:')
+// console.log(25 - '10');
 
-console.log('Comparação de valores:')
-console.log('1. Compara com coerção:')
-console.log("10 == '10'");
-console.log(10 == '10');
-console.log('2. Comparação sem coreção:')
-console.log("10 === '10'");
-console.log(10 === '10');
+// console.log('Comparação de valores:')
+// console.log('1. Compara com coerção:')
+// console.log("10 == '10'");
+// console.log(10 == '10');
+// console.log('2. Comparação sem coreção:')
+// console.log("10 === '10'");
+// console.log(10 === '10');
+
+// Boolean (true/false)
+const nome = '';
+
+if (nome) {
+    console.log('preenchido');
+};
+
+
+// Null & Undefined
+
+// Null -> ausência de valor, e não a inexistência da propriedade.
+// Undefined -> como se a variável não existisse, ausência de declaração.
+// const x = null;
+// let y;
+
+// console.log(x);
+// console.log(y);
+
+
+// const x = { nome: null };
+// console.log(x.nome, x.idade);
+
+
+const x = { nome: null }
+
+console.log(JSON.stringify(x));
 
